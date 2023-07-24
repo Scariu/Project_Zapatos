208aa83153d598d185be868d0ff0b8547edf83f2
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-23 23:30:08 -0400

    Se crea item_cart.xml y se crea diseño.

--- app/src/main/java/com/example/project_zapatos/ThirdFragmentCart.kt
+++ app/src/main/java/com/example/project_zapatos/ThirdFragmentCart.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.project_zapatos.databinding.FragmentSecondBinding
+import com.example.project_zapatos.databinding.FragmentThirdCartBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +19,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ThirdFragmentCart : Fragment() {
+    private lateinit var binding: FragmentThirdCartBinding
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -33,8 +36,8 @@ class ThirdFragmentCart : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third_cart, container, false)
+        binding = FragmentThirdCartBinding.inflate(LayoutInflater.from(activity))
+        return (binding.root)
     }
 
     companion object {
